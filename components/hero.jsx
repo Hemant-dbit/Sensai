7f130f4b52a59ece3dd5679057a714e8c5dbfbb3
Hemant <guptahemant350@gmail.com>
2025-05-05 23:30:12 +0530

    Hero:Banner,transition

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,14 +1,37 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { useEffect, useRef } from "react";
 
 function HeroSection() {
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <section className="w-full pt-36 md:pt-48 pb-10">
       <div className="space-y-6 text-center">
         <div className="space-y-6 mx-auto">
-        <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl bg-gradient-to-b from-gray-400 via-gray-200 to-gray-600 font-extrabold tracking-tighter text-transparent bg-clip-text pb-2 pr-2">
+          <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl bg-gradient-to-b from-gray-400 via-gray-200 to-gray-600 font-extrabold tracking-tighter text-transparent bg-clip-text pb-2 pr-2">
             Your AI Career Coach for
             <br />
             Professional Success
@@ -39,14 +62,14 @@ function HeroSection() {
             </Button>
           </Link>
         </div>
-        <div className="hero-image-wrapper mt-5 md:mt-0">
-          <div className="hero-image">
+        <div className="hero-image-wrapper mt-5 md:mt-0 mb-10">
+          <div ref={imageRef} className="hero-image ">
             <Image
-              src="/banner3.jpeg"
+              src="/banner.jpeg"
               width={1100}
               height={500}
               alt="Dashboard Preview"
-              className="rounded-lg shadow-2xl border mx-auto"
+              className="rounded-lg shadow-2xl border mx-auto h-auto"
               priority
             />
           </div>
