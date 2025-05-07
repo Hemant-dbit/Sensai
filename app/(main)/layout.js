e384d6f02a5c4738fdb5d67eccd44fe19a2e4ce4
Hemant <guptahemant350@gmail.com>
2025-05-07 16:15:35 +0530

    MainLayout

---
+++ app/(main)/layout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainLayout = ({ children }) => {
+  return <div className="container mb-20 mx-auto mt-20 text-center">{children}</div>;
+};
+
+export default MainLayout;
