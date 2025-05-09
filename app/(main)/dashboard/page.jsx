aa88a3f2c3abcac11ca295790b3682893a59258a
Hemant <guptahemant350@gmail.com>
2025-05-09 13:28:32 +0530

    Dashboard: Completed

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { redirect } from 'next/navigation'
-import { getUserOnBoardingStatus } from '@/actions/user'
+import React from "react";
+import { redirect } from "next/navigation";
+import { getUserOnBoardingStatus } from "@/actions/user";
+import DashboardView from "./_components/dashboard-view";
+import { getIndustryInsights } from "@/actions/dashboard";
 
 const IndustryInsightsPage = async () => {
   const { isOnBoarded } = await getUserOnBoardingStatus();
-  
-    if (!isOnBoarded) {
-      redirect('/onboarding')
-    }
-  return (
-    <div>IndustryInsightsPage</div>
-  )
-}
+  const insights = await getIndustryInsights();
 
-export default IndustryInsightsPage
+  if (!isOnBoarded) {
+    redirect("/onboarding");
+  }
+  return (
+    <div className="mx-auto container">
+      <DashboardView insights={insights}/>
+    </div>
+  );
+};
+
+export default IndustryInsightsPage;
