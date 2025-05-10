63b78f00ae0c3dabdb4cada5942b64ac7a34afa6
Hemant <guptahemant350@gmail.com>
2025-05-10 16:16:10 +0530

    MockInterview: Header and Quiz

--- app/(main)/interview/mock/page.jsx
+++ app/(main)/interview/mock/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Quiz from "../_components/quiz";
 
-const MockInterviewPage = () => {
+export default function MockInterviewPage() {
   return (
-    <div>MockInterviewPage</div>
-  )
-}
+    <div className="flex flex-col space-y-2 mx-2 items-start text-left">
 
-export default MockInterviewPage
+      <div className="flex flex-col space-y-2 mx-2">
+        <Link href="/interview">
+          <Button className="gap-2 pl-0" >
+            <ArrowLeft className="h-4 w-4" />
+            Back to Interview Preparation
+          </Button>
+        </Link>
+
+        <div>
+          <h1 className="text-6xl font-bold gradient-title">Mock Interview</h1>
+          <p className="text-muted-foreground">
+            Test your knowledge with industry-specific questions
+          </p>
+        </div>
+      </div>
+
+      <Quiz />
+    </div>
+  );
+}
