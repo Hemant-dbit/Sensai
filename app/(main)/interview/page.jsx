12b7d1687772855f5e5054820be62e04f9ca4e0e
Hemant <guptahemant350@gmail.com>
2025-05-14 14:23:45 +0530

    Interview Page: Updated

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { getAssessments } from "@/actions/interview";
+import PerformanceChart from "./_components/performance-chart";
+import Statscard from "./_components/stats-cards";
+import QuizList from "./_components/quiz-list";
 
-const InterviewPage = () => {
+
+const InterviewPage = async() => {
+
+  const assessments = await getAssessments();
   return (
-    <div>InterviewPage</div>
-  )
-}
+    <div>
+      <h1 className="text-6xl font-bold text-left gradient-title mb-5">
+        Interview Preparation
+      </h1>
 
-export default InterviewPage
+      <div className="space-y-6">
+        <Statscard assessments={assessments}/>
+        <PerformanceChart assessments={assessments} />
+        <QuizList assessments={assessments} />
+      </div>
+    </div>
+  );
+};
+
+export default InterviewPage;
